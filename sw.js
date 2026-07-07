a09718a309d071c28bef4110bdeab70e2957d47c
efim26470-ops <efim26470@gmail.com>
2026-07-08 01:06:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,25 @@
-const CACHE = 'lifeos-v9-complete-20260707-2';
-const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
-self.addEventListener('install', event => {
+const CACHE = 'lifeos-v10-stable-20260707-1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
+self.addEventListener('install', function(event){
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  event.waitUntil(caches.open(CACHE).then(function(cache){
+    return Promise.all(CORE.map(function(url){return cache.add(url).catch(function(){return null;});}));
+  }));
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+self.addEventListener('activate', function(event){
+  event.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k !== CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
 });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.origin !== location.origin) return;
-  event.respondWith(fetch(event.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+self.addEventListener('fetch', function(event){
+  if(event.request.method !== 'GET') return;
+  var url = new URL(event.request.url);
+  if(url.origin !== location.origin) return;
+  event.respondWith(fetch(event.request).then(function(res){
+    var copy = res.clone();
+    caches.open(CACHE).then(function(cache){ cache.put(event.request, copy).catch(function(){}); });
     return res;
-  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
+  }).catch(function(){
+    return caches.match(event.request).then(function(cached){ return cached || caches.match('./index.html'); });
+  }));
 });
